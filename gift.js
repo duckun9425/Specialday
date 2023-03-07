4fa66d0666a82e41361454393a064ec3a0640a94
duckun9425 <106146694+duckun9425@users.noreply.github.com>
2023-03-07 19:11:45 +0700

    Update gift.js

--- gift.js
+++ gift.js
@@ -1,5 +1,5 @@
 var to = 'My Teacher';
-var gift_url = 'https://duckun9425.github.io/Heart/';
+var gift_url = 'https://duckun9425.github.io/Specialday/?fbclid=IwAR2PVlYANbcQVHxAzTkcdcz4WNpVc_kO6eXD-pjA4tC5tLJCNQWFf-V5A6c';
 var gift_image_url = 'https://scontent.fsgn8-2.fna.fbcdn.net/v/t1.6435-9/60704178_865603677124679_1705792234936336384_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=go5nvDL7vlMAX9WwZrq&_nc_ht=scontent.fsgn8-2.fna&oh=00_AfAbk1D27IKOdLbmSAsvoKIRHok-Reo4zimGBpWZ4fkGgA&oe=6423C875';
 
 
